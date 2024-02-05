39a597973d257b00f7d1684b7311b650895ca22b
OguzhanSaman <oguzhan.saman@themos.com.tr>
2024-02-05 16:30:39 +0300

    asd

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -153,7 +153,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "816b1f244e482e2a"; }
+/******/ 		__webpack_require__.h = function() { return "a97482c6c6f53c5c"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
