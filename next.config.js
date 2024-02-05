2c4b59af7c178f48a84c144114715a102ecabb67
OguzhanSaman <oguzhan.saman@themos.com.tr>
2024-02-05 13:59:51 +0300

    comm

--- next.config.js
+++ next.config.js
@@ -1,13 +1,14 @@
-/** @type {import('next').NextConfig} */
-
+// next.config.js
 const nextConfig = {
   reactStrictMode: true,
-  basePath: process.env.NEXT_PUBLIC_BASE_PATH || '/oguzhansaman.github.io',
-  assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH || '/oguzhansaman.github.io',
-  exportPathMap: function () {
-    return {
-      '/': { page: '/' },
-    };
+  basePath: process.env.NEXT_PUBLIC_BASE_PATH || '',
+  assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH || '',
+  output: {
+    publicPath: process.env.NEXT_PUBLIC_BASE_PATH || '',
+  },
+  generateBuildId: async () => {
+    // You can leave this function empty or customize it if needed
+    return 'build';
   },
 };
 
