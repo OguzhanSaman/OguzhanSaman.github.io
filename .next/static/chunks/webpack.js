9cd0b4c36361ccce722c2d903386a00830a27f47
OguzhanSaman <oguzhan.saman@themos.com.tr>
2024-04-22 17:26:50 +0300

    news

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "7352aada4cf2e4c7"; }
+/******/ 		__webpack_require__.h = function() { return "7fe0b02f56524ff1"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- .next/static/webpack/webpack.e168172fca6efdef.hot-update.js
+++ .next/static/webpack/webpack.e168172fca6efdef.hot-update.js
@@ -11,7 +11,7 @@ self["webpackHotUpdate_N_E"]("webpack",{},
 /******/ function(__webpack_require__) { // webpackRuntimeModules
 /******/ /* webpack/runtime/getFullHash */
 /******/ !function() {
-/******/ 	__webpack_require__.h = function() { return "b747d7590d72869a"; }
+/******/ 	__webpack_require__.h = function() { return "18099205c814a408"; }
 /******/ }();
 /******/ 
 /******/ }
